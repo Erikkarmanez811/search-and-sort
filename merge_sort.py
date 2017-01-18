d5f1f4e11874c54a27212b7755629b88964c928b
Alexander Avdoshkin <alexavdoshkin@gmail.com>
2017-01-18 10:42:22 +0300

    merge sort added

--- merge_sort.py
+++ merge_sort.py
@@ -1,3 +1,22 @@
 def merge(ar,ra):
+    i = 0
+    j = 0
+    re= []
+    while i != len(ar) and j != len(ra):
+        if ar[i] < ra[j]:
+            re.append(ar[i])
+            i += 1
+        else:
+            re.append(ra[j])
+            j += 1
+    re += ar[i:] + ra[j:]
+    return re
+
+
+
+def merge_sort(ar):
+    if len(ar) == 1:
+        return ar
+    return merge(merge_sort(ar[:len(ar)//2]),merge_sort(ar[len(ar)//2:]))
     
-    
+
